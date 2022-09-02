e793d54b8c17921e8110bb045f186ab1870e8e44
Juan Rojas <juan@Juans-MacBook-Pro.local>
2022-09-01 23:10:01 -0400

    🧪 git new mac

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 const IndexPage = () => {
   return (
     <main className=" bg-slate-500">
-      <h1>Lorem Ipsum</h1>
+      <h1>Lorem Ipsum sample</h1>
       <p>Lorem ipsum dolores de la vaca</p>
     </main>
   );
